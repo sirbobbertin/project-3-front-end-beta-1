23236e0fe0a3090664fa3d4111d2fbea04b5494f
sirbobbertin <anthonyjjohnson1999@gmail.com>
2022-01-08 16:27:36 -0600

    Testing W.I.P, store-product

--- src/app/shop/store-product/store-product.component.spec.ts
+++ src/app/shop/store-product/store-product.component.spec.ts
@@ -1,16 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { StoreProductComponent } from './store-product.component';
+import { ProductService } from 'src/app/services/product.service';
+import { UserService } from 'src/app/services/user.service';
+import { FormBuilder, FormsModule } from '@angular/forms';
 
 describe('StoreProductComponent', () => {
   let component: StoreProductComponent;
   let fixture: ComponentFixture<StoreProductComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let productService: ProductService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StoreProductComponent ]
+      imports: [HttpClientTestingModule,RouterTestingModule,FormsModule],
+      declarations: [ StoreProductComponent ],
+      providers: [ProductService,FormBuilder]
     })
     .compileComponents();
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    productService = TestBed.inject(ProductService);
   });
 
   beforeEach(() => {
