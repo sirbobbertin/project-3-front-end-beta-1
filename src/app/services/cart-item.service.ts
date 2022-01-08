726f97ebc47bd5e0dcc4ef3b78ac2d89e19c7b74
Michelle Harper <michelle.ym.harper@gmail.com>
2022-01-08 11:00:24 -0800

    Revert "revert changes"
    This reverts commit f20263d9344ef7b12f231fbf5e7f0277c525b55c.

--- src/app/services/cart-item.service.ts
+++ src/app/services/cart-item.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { CartItem } from '../models/cart.model';
-import { Instance } from "../models/Instance";
+import {Instance} from "../models/Instance";
 
 
 @Injectable({
@@ -10,19 +10,16 @@ import { Instance } from "../models/Instance";
 })
 
 export class CartItemService {
-  getCartAndItemsWithUserIdService() {
-    throw new Error('Method not implemented.');
-  }
 
   baseUrl = Instance.url + "/api/cart-items";
 
   constructor(private http: HttpClient) { }
 
-  addNewItemService(item: CartItem): Observable<CartItem> {
+  addNewItemService(item: CartItem): Observable<CartItem>{
     return this.http.post<CartItem>(this.baseUrl, item);
   }
 
-  updateItemService(item: CartItem): Observable<CartItem> {
+  updateItemService(item: CartItem): Observable<CartItem>{
     return this.http.put<CartItem>(this.baseUrl, item);
   }
 
