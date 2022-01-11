5f4792431c6d3ce867a42d6fa9ba21573437d93b
Rowel Selidio <rselidio@usa.edu.ph>
2022-01-10 22:45:39 -0700

    final testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('rev-tech');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rev-tech app is running!');
-  });
+ 
 });
